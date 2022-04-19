817c6e24185a7315838443c4e977524769759271
turboschnek <brno.kubaurb@gmail.com>
2022-04-19 23:54:00 +0200

    functions description

--- src/ai.h
+++ src/ai.h
@@ -9,25 +9,92 @@
 
 
 #include "chess_net.h"
+#include "chess_structs.h"
+
+
+/**
+ * Initializes population of chNets and evolves them by forcing them to fight
+ * each other in the most deadly game of chess in their lives.
+ * 
+ * saves the population
+ */
+void chNetEvolution();
 
 /**
  * lets two AIs play game of chess
  * 
  * uses networks in parameters to staticaly evaluate positions reached
- * by minimax algo
+ * by minimax algo. If chNet is NULL, primitiveEval is used instead. 
  * 
  * @return 0 for draw, 1 for win of white, -1 for win of black
  */
-int game(TchNet* white, TchNet* black, float timeBudget);
+int game(const TchNet* white, const TchNet* black, float timeBudget);
 
-/*
-TODO:
-  standard minimax with primitive evaluation from my past project
-  minimax with use of neural net
-  tournament (objective is not to find the best one...
-              ...It is to find the better half)
-  evolution where the tournament is the fitness function
-  tinker with layerCounts and other stuff
-*/
+
+
+/**
+ * sorts population, second half is sentenced to death
+ */
+void quickTournament(TchNet** population, int populationCount);
+
+/**
+ * Uses minimax to choose a move.
+ * 
+ * @param b current board
+ * @param net chess network used for evaluation (if NULL, primitiveEval is used)
+ * @param output gets filled by AI (length == MAX_INP_LENGTH)
+ * @param seconds max time for move
+ * 
+ * @return depth of finished search
+ */
+int minimax(Tboard *b, const TchNet* net, float seconds, char *output);
+
+
+/**
+ * recursive part of minimax alg
+ * 
+ * @param b board position to evaluate
+ * @param net chess network used for evaluation (if NULL, primitiveEval is used)
+ * @param depth depth of search (recomended odd value)
+ * @param isMax is player maximazing?
+ * @param alfa for pruning (initially -INF)
+ * @param beta for pruning (initially INF)
+ * 
+ * @return numerical evaluation of position
+ * @note use minimax() instead
+ */
+int inner_minimax(Tboard *b, TchNet* net, int depth, bool isMax, float alfa, float beta);
+
+
+/**
+ * sorts ml based on keys` values
+ */
+void sortPopulation(TchNet** population, int *keys, bool increasing);
+
+
+/**
+ * sorts ml based on keys` values
+ */
+void sortMoveList(TmoveList* ml, int *keys, bool increasing);
+
+
+/**
+ * sum of piece values from fun getPieceValue
+ */
+float primitiveEval(const char* posString);
+
+/**
+ * 
+ * Gets value of piece.  
+ * Position is considered for knight, pawn and king.
+ * 
+ * @param piece ex.: 'k', 'K', 'n'
+ * @param col piece's column (starting on left)
+ * @param row piece's row (starting on top)
+ * @param pieceCount number of remaining pieces on board
+ * 
+ * @return value between -900(Q) and 900(q)
+ */
+int getPieceValue(char piece, int row, int col, int pieceCount);
 
 #endif
